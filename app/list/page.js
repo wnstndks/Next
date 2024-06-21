a9c7271c616b7f78ec50747f8c2fc1e6af823e7b
wnstndks <anjs9912@gmail.com>
2024-06-21 14:30:27 +0900

    page routing

---
+++ app/list/page.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function List() {
+    return (
+      <div>
+        <h2>Products</h2>
+        <div className="food">
+          <h4>상품명 $40</h4>
+        </div>
+        <div className="food">
+          <h4>상품명 $40</h4>
+        </div>
+      </div>
+    )
+  } 
