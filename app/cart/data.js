31648ee2544b6451357bc6ed35998cbc00c67933
wnstndks <anjs9912@gmail.com>
2024-06-23 00:48:24 +0900

    export component

---
+++ app/cart/data.js
@@ -0,0 +1,12 @@
+// let age=20;
+// let name='kim';
+// export 는 한개만 가능
+// export default age;
+// export {age,name}
+
+function Hello(){
+    return(
+        <h1>안녕하세요</h1>
+    )
+}
+export default Hello;
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,36 @@
+import Hello from './data.js'
+// export 내 대괄호 작성시 작명안됨- 그것과 똑같게
+// import {age,name} from './data.js'
+// ./는 현재 경로라는 의미
+// ../는 내 상위 폴더로 이동하라는 의미
+
 export default function Cart() {
     return (
       <div>
         <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
+        <CartItem/>
+        <CartItem/>
+        <CartItem/>
+        <Hello/>
       </div>
     )
   } 
+
+  // component 만드는 법
+  // 1. function 작명(){}
+  // 2. return(축약할 긴 HTML)
+  // 3. <작명/>사용
+  function CartItem(){
+    return(
+      <div className="cart-item">
+          <p>상품명</p>
+          <p>$40</p>
+          <p>1개</p>
+        </div>
+    )
+
+  }
+
+  // server component - 아무데서나 대충만든것 - html에 자바스크립트 기능넣기 불가능 useState, useeEffect 사용불가
+  // client component - 파일 맨 위에 'use client' 대고 만든것 - 위의 것 사용 가능
+  // 큰 페이지는 server, js기능 필요한 곳만 client component
