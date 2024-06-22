bce4da456029a164ec95755e63953c4f15a1949c
wnstndks <anjs9912@gmail.com>
2024-06-22 22:54:52 +0900

    레이아웃

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h4 className="title">애플후레시</h4>
+    <main>
+      <h1 className="title">Programming Log</h1>
       <p className="title-sub">by dev kim</p>
-    </div>
+    </main>
   );
 }
