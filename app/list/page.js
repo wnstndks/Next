a01707bff461d6abfa0c5361e3004336b606a20a
wnstndks <anjs9912@gmail.com>
2024-06-22 22:59:57 +0900

    map

--- app/list/page.js
+++ app/list/page.js
@@ -1,15 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function List() {
-    return (
-      <div>
-        <h2>Products</h2>
-        <div className="food">
-          <h4>상품명 $40</h4>
-        </div>
-        <div className="food">
-          <h4>상품명 $40</h4>
-        </div>
-      </div>
-    )
-  } 
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+  return (
+    <div>
+      <h4 className="title">상품목록</h4>
+      {
+        상품.map((a,i)=>{
+          return (
+            <div className="food">
+              <h4>{a}</h4>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
