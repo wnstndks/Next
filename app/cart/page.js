9feb10bdc62a2f05b60007c4a791525f0785e0f7
wnstndks <anjs9912@gmail.com>
2024-06-24 22:57:40 +0900

    props(react랑 same)

--- app/cart/page.js
+++ app/cart/page.js
@@ -5,25 +5,36 @@ import Hello from './data.js'
 // ../는 내 상위 폴더로 이동하라는 의미
 
 export default function Cart() {
+  let 장바구니 = ['Iphone','Galaxy']
     return (
       <div>
         <h4 className="title">Cart</h4>
-        <CartItem/>
-        <CartItem/>
-        <CartItem/>
-        <Hello/>
+        <CartItem item={장바구니[0]}/>
+        <CartItem item={장바구니[1]}/>
+        {/* <Hello/> */}
+        <Banner content="롯데카드"/>
+        <Btn color="red"/>
+        <Btn color="blue"/>
       </div>
     )
   } 
+
+function Banner(props){
+  return <h5>{props.content} 결제 행사중</h5>
+}
+
+function Btn(props){
+  return <button style={{backgroundColor:props.color}}>{props.color} 버튼</button>
+}
 
   // component 만드는 법
   // 1. function 작명(){}
   // 2. return(축약할 긴 HTML)
   // 3. <작명/>사용
-  function CartItem(){
+  function CartItem(props){
     return(
       <div className="cart-item">
-          <p>상품명</p>
+          <p>{props.item}</p>
           <p>$40</p>
           <p>1개</p>
         </div>
@@ -34,3 +45,6 @@ export default function Cart() {
   // server component - 아무데서나 대충만든것 - html에 자바스크립트 기능넣기 불가능 useState, useeEffect 사용불가
   // client component - 파일 맨 위에 'use client' 대고 만든것 - 위의 것 사용 가능
   // 큰 페이지는 server, js기능 필요한 곳만 client component
+
+
+  // 데이터가 많은 컴포넌트에서 필요하면 데이터를 그들 중 최고 부모 컴포넌트에 보관해야 좋음
