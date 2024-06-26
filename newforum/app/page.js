a1fd07da24338c4626f0548718c20cd37a3ff49c
wnstndks <anjs9912@gmail.com>
2024-06-26 17:33:30 +0900

    mongodb setting

--- newforum/app/page.js
+++ newforum/app/page.js
@@ -1,5 +1,16 @@
+import { connectDB } from "@/util/database";
+// 자바스크립트 파일끼리는 .js 생략해도 무방
+import { MongoClient } from "mongodb";
 
-export default function Home() {
+
+export default async function Home() {
+  // mongodb 연결_DB 입출력 코드는 server component 안에서만 사용하기
+  const client = await connectDB;
+  const db = client.db("nextforum")
+  let result= await db.collection('post').find().toArray()
+  console.log(result)
+
+
   return (
     <div>안녕하세용</div>
   );
---
+++ newforum/util/database.js
@@ -0,0 +1,16 @@
+// mongodb 연결
+import { MongoClient } from 'mongodb'
+const url = 'mongodb+srv://admin:dwjtk2718@cluster0.nmfemuv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const options = { useNewUrlParser: true }
+let connectDB
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url, options).connect()
+  }
+  connectDB = global._mongo
+} else {
+  connectDB = new MongoClient(url, options).connect()
+}
+export { connectDB }
