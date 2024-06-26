bf6471add4cd4168fac0326a2fd5bd3e4eec8295
wnstndks <anjs9912@gmail.com>
2024-06-27 00:58:18 +0900

    design 잘해보자

--- newforum/app/write/page.js
+++ newforum/app/write/page.js
@@ -6,11 +6,13 @@ export default function Write() {
     <div>
       <h4>글 작성할거냐</h4>
       {/* action이라는 속성 내에 url 쓰고, method에 POST 속성 */}
-      <form action="/api/write" method="POST">
+      <div className="p-20">
+        <form action="/api/write" method="POST">
           <input name="title" placeholder="글제목"></input>
           <input name="content" placeholder="글내용"></input>
-        <button type="submit">버튼</button>
-      </form>
+          <button type="submit">버튼</button>
+        </form>
+      </div>
     </div>
   );
 }
--- newforum/pages/api/write.js
+++ newforum/pages/api/write.js
@@ -1,14 +1,21 @@
-import { connectDB } from "@/util/database"
-import { useState } from "react"
+import { connectDB } from "@/util/database";
 
-export default async function writehandler(요청,응답){
-
-    if(요청.method=='POST'){
-        const db = (await connectDB).db("nextforum")
-        let {title,content}=요청.body;
-        let result= await db.collection('post').insertOne(요청.body)
-        
-        // 리다이렉트
-        return 응답.status(200).redirect('/list')
+// 유저 -> 서버 -> DB 이런 식으로 개발하기
+// 서버기능은 /api 폴더에
+export default async function writehandler(요청, 응답) {
+  if (요청.method == "POST") {
+    if (요청.body.title == "") {
+      return 응답.status(500).json("제목 빨리 써라");
     }
+    // DB 에러 예외처리는 try-catch 문법 사용
+    try {
+      const db = (await connectDB).db("nextforum");
+      let result = await db.collection("post").insertOne(요청.body);
+
+      // 리다이렉트
+      return 응답.status(200).redirect("/list");
+    } catch (error) {
+        return 응답.status(500).json(error)
+    }
+  }
 }
